c82bc0f294fe2d8aeffdcc643f0b6652d0c9d825
H. Edmundo Ramirez <edmundormz@gmail.com>
2018-03-22 20:48:56 -0600

    Function skeleton

---
+++ madera.py
@@ -0,0 +1,29 @@
+import sys
+
+
+def aserrio():
+    pass
+
+
+def raja():
+    pass
+
+
+def troncos():
+    pass
+
+
+def celulosas():
+    pass
+
+
+if __name__ == "__main__":
+    producto = sys.argv[1]
+    if producto == "aserrio":
+        aserrio()
+    elif producto == "raja":
+        raja()
+    elif producto == "troncos":
+        troncos()
+    elif producto == "celulosas":
+        celulosas()
