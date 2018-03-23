7c899982c3afafeefec5f17fef2f7d10941f0867
Edmundo Ramirez <hector.edmundox.ramirez.gomez@intel.com>
2018-03-23 14:58:25 -0600

    Added more tests

--- madera.py
+++ madera.py
@@ -3,99 +3,102 @@ import sys
 
 
 def selector(producto, ancho, largo, alto=None, clase=None, piezas=None):
-	if producto == "raja":
-		raja(ancho, largo, alto)
-	elif producto == "aserrio":
-		aserrio(ancho, alto, largo, clase)
-	elif producto == "troncos":
-		troncos(ancho, largo, piezas)
-	elif producto == "aserrin" or producto == "astilla":
-		celulosas(producto,ancho, largo, alto)
+    if producto == "raja":
+        raja(ancho, largo, alto)
+    elif producto == "aserrio":
+        aserrio(ancho, alto, largo, clase)
+    elif producto == "troncos":
+        troncos(ancho, largo, piezas)
+    elif producto == "aserrin" or producto == "astilla":
+        celulosas(producto, ancho, largo, alto)
 
 
 def raja(ancho, largo, altos):
-	"""
-	Calcula el volumen de una carga de raja, promediando 3 medidas de la
-	altura y usando un factor de conversion
+    """
+    Calcula el volumen de una carga de raja, promediando 3 medidas de la
+    altura y usando un factor de conversion
 
-	:param ancho: ancho de la carga, en metros
-	:param largo: largo de la carga, en metros
-	:param altos: lista con n alturas, en metros
-	:return: tupla con volumen de carga y precio de la misma
-	"""
-	alto = 0
-	for a in altos:
-		alto += a
-	alto = alto/len(altos)
-	volumen = ancho * alto * largo
-	vol_str = "{0:.3f}".format(volumen)
-	precio = volumen * 0.65 * 320
-	precio_str = "{0:.2f}".format(precio)
-	print(float(vol_str), float(precio_str))
-	return float(vol_str), float(precio_str)
+    :param ancho: ancho de la carga, en metros
+    :param largo: largo de la carga, en metros
+    :param altos: lista con n alturas, en metros
+    :return: tupla con volumen de carga y precio de la misma
+    """
+    alto = 0
+    for a in altos:
+        alto += a
+    alto = alto / len(altos)
+    volumen = ancho * alto * largo
+    vol_str = "{0:.3f}".format(volumen)
+    precio = volumen * 0.65 * 320
+    precio_str = "{0:.2f}".format(precio)
+    print(float(vol_str), float(precio_str))
+    return float(vol_str), float(precio_str)
+
 
 def aserrio(ancho, alto, largo, clase):
-	"""
-	Calcula precio de una pieza de madera aserrada (tabla, polin, viga) de
-	acuerdo a su clasificacion de calidad (primera, segunda o tercera)
-	:param ancho: ancho de la pieza, en pulgadas
-	:param alto: alto de la pieza, en pulgadas
-	:param largo: largo de la pieza, en pies
-	:param clase: calidad de la pieza (1, 2 o 3), determina el precio
-	:return: string con descripcion y precio de la pieza
-	"""
-	volumen = ancho * largo * alto / 12
-	if clase == 1:
-		precio = volumen * 15
-	if clase == 2:
-		precio = volumen * 12
-	if clase == 3:
-		precio = volumen * 10
-	nombre = '{0} x {1} x {2} {3}a, ${4}'.format(str(ancho), str(alto),
-												str(largo), str(clase),
-												 str(precio))
-	print nombre
-	return nombre
+    """
+    Calcula precio de una pieza de madera aserrada (tabla, polin, viga) de
+    acuerdo a su clasificacion de calidad (primera, segunda o tercera)
+    :param ancho: ancho de la pieza, en pulgadas
+    :param alto: alto de la pieza, en pulgadas
+    :param largo: largo de la pieza, en pies
+    :param clase: calidad de la pieza (1, 2 o 3), determina el precio
+    :return: string con descripcion y precio de la pieza
+    """
+    volumen = ancho * largo * alto / 12
+    if clase == 1:
+        precio = volumen * 15
+    if clase == 2:
+        precio = volumen * 12
+    if clase == 3:
+        precio = volumen * 10
+    nombre = '{0} x {1} x {2} {3}a, ${4}'.format(str(ancho), str(alto),
+                                                 str(largo), str(clase),
+                                                 str(precio))
+    print nombre
+    return nombre
+
 
 def troncos(diametro, longitud, piezas=1):
-	"""
-	Calcula el volumen de troncos de madera
-	:param diametro: diametro de la pieza, en centimetros
-	:param longitud: longitud de la pieza, en metros
-	:param piezas: cantidad de piezas que comparten diametro y longitud para
-	calcular sus volumenes
-	:return: volumen del total de troncos calculados
-	"""
-	radio_metros = (float(diametro)/100)/2
-	area = math.pi * (radio_metros**2)
-	volumen =  area * longitud * piezas
-	vol_str = "{0:.3f}".format(volumen)
-	print vol_str
-	return float(vol_str)
+    """
+    Calcula el volumen de troncos de madera
+    :param diametro: diametro de la pieza, en centimetros
+    :param longitud: longitud de la pieza, en metros
+    :param piezas: cantidad de piezas que comparten diametro y longitud para
+    calcular sus volumenes
+    :return: volumen del total de troncos calculados
+    """
+    radio_metros = (float(diametro) / 100) / 2
+    area = math.pi * (radio_metros ** 2)
+    volumen = area * longitud * piezas
+    vol_str = "{0:.3f}".format(volumen)
+    print vol_str
+    return float(vol_str)
 
 
 def celulosas(producto, ancho, largo, alto):
-	"""
-	Calcula volumen y costo de cargas de productos para celulosa,
-	como aserrin o astilla
-	:param producto: tipo de producto [aserrin, astilla] para definir precio
-	:param ancho: ancho de la carga, en metros
-	:param largo: largo de la carga, en metros
-	:param alto: longitud de la carga, en metros
-	:return: tupla que contiene volumen calculado y precio del mismo
-	"""
-	volumen = ancho * largo * alto
-	if producto == "astilla":
-		precio = volumen * 240
-	elif producto == "aserrin":
-		precio = volumen * 180
-	else:
-		msj = "Producto incorrecto"
-		print msj
-		return msj
-	vol_str = "{0:.3f}".format(volumen)
-	print(vol_str, precio)
-	return float(vol_str), precio
+    """
+    Calcula volumen y costo de cargas de productos para celulosa,
+    como aserrin o astilla
+    :param producto: tipo de producto [aserrin, astilla] para definir precio
+    :param ancho: ancho de la carga, en metros
+    :param largo: largo de la carga, en metros
+    :param alto: longitud de la carga, en metros
+    :return: tupla que contiene volumen calculado y precio del mismo
+    """
+    volumen = ancho * largo * alto
+    if producto == "astilla":
+        precio = volumen * 240
+    elif producto == "aserrin":
+        precio = volumen * 180
+    else:
+        msj = "Producto incorrecto"
+        print msj
+        return msj
+    vol_str = "{0:.3f}".format(volumen)
+    print(vol_str, precio)
+    return float(vol_str), precio
+
 
 if __name__ == "__main__":
-	selector(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+    selector(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
--- test_madera.py
+++ test_madera.py
@@ -1,8 +1,9 @@
 import unittest
+
 import madera
 
 
-class TestMadera(unittest.TestCase):
+class TestCelulosas(unittest.TestCase):
     def test_celulosas_aserrin(self):
         self.assertEqual(madera.celulosas("aserrin", 2.4, 6.3, 2.8),
                          (42.336, 7620.48))
@@ -14,3 +15,35 @@ class TestMadera(unittest.TestCase):
     def test_celulosas_invalido(self):
         self.assertEqual(madera.celulosas("bombones", 0, 0, 0),
                          "Producto incorrecto")
+
+
+class TestTroncos(unittest.TestCase):
+    def test_troncos_no_pieces(self):
+        self.assertEqual(madera.troncos(50, 2.44), 0.479)
+
+    def test_troncos_two_pieces(self):
+        self.assertEqual(madera.troncos(50, 2.44, 2), 0.958)
+
+
+class TestAserrio(unittest.TestCase):
+    def test_aserrio_1a(self):
+        self.assertEqual(madera.aserrio(4, 0.75, 8, 1),
+                         "4 x 0.75 x 8 1a, $30.0")
+
+    def test_aserrio_2a(self):
+        self.assertEqual(madera.aserrio(4, 0.75, 8, 2),
+                         "4 x 0.75 x 8 2a, $24.0")
+
+    def test_aserrio_3a(self):
+        self.assertEqual(madera.aserrio(4, 0.75, 8, 3),
+                         "4 x 0.75 x 8 3a, $20.0")
+
+
+class TestRaja(unittest.TestCase):
+    def test_raja_1_alto(self):
+        self.assertEqual(madera.raja(2.4, 6.3, [2.3]),
+                         (34.776, 7233.41))
+
+    def test_raja_3_altos(self):
+        self.assertEqual(madera.raja(2.4, 6.3, [2.3, 2.6, 2]),
+                         (34.776, 7233.41))
