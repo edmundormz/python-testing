25b5782de0c2e3ad716f2eb9b7d71646adb0786c
Edmundo Ramirez <hector.edmundox.ramirez.gomez@intel.com>
2018-03-23 12:10:06 -0600

    Added madera.aserrrio() logic, modified TestMadera structure

--- madera.py
+++ madera.py
@@ -1,9 +1,27 @@
 import sys
 
 
-def aserrio():
-    pass
-
+def aserrio(ancho, alto, largo, clase):
+    """
+    Calcula precio de una pieza de madera aserrada (tabla, polin, viga) de
+    acuerdo a su clasificacion de calidad (primera, segunda o tercera)
+    :param ancho: ancho de la pieza, en pulgadas
+    :param alto: alto de la pieza, en pulgadas
+    :param largo: largo de la pieza, en pies
+    :param clase: calidad de la pieza (1, 2 o 3), determina el precio
+    :return: string con descripcion y precio de la pieza
+    """
+    volumen = ancho * largo * alto / 12
+    if clase == 1:
+        precio = volumen * 15
+    if clase == 2:
+        precio = volumen * 12
+    if clase == 3:
+        precio = volumen * 10
+    nombre = '{0} x {1} x {2} {3}a, ${4}'.format(str(ancho), str(alto),
+                                                str(largo), str(clase),
+                                                 str(precio))
+    return nombre
 
 def raja():
     pass
@@ -14,6 +32,15 @@ def troncos():
 
 
 def celulosas(producto, ancho, largo, alto):
+    """
+    Calcula volumen y costo de cargas de productos para celulosa,
+    como aserrin o astilla
+    :param producto: tipo de producto [aserrin, astilla] para definir precio
+    :param ancho: ancho de la carga, en metros
+    :param largo: largo de la carga, en metros
+    :param alto: longitud de la carga, en metros
+    :return: tupla que contiene volumen calculado y precio del mismo
+    """
     volumen = ancho * largo * alto
     if producto == "astilla":
         precio = volumen * 240
--- test_madera.py
+++ test_madera.py
@@ -3,7 +3,14 @@ import madera
 
 
 class TestMadera(unittest.TestCase):
-    def test_celulosas(self):
-        self.assertEqual(madera.celulosas("aserrin", 2.4, 6.3, 2.8), (42.336, 7620.48))
-        self.assertEqual(madera.celulosas("astilla", 2.4, 6.3, 2.8), (42.336, 10160.64))
-        self.assertEqual(madera.celulosas("bombones", 0, 0, 0), "Producto incorrecto")
+    def test_celulosas_aserrin(self):
+        self.assertEqual(madera.celulosas("aserrin", 2.4, 6.3, 2.8),
+                         (42.336, 7620.48))
+
+    def test_celulosas_astilla(self):
+        self.assertEqual(madera.celulosas("astilla", 2.4, 6.3, 2.8),
+                         (42.336, 10160.64))
+
+    def test_celulosas_invalido(self):
+        self.assertEqual(madera.celulosas("bombones", 0, 0, 0),
+                         "Producto incorrecto")
